18282ccc28fcb68f9de961b32cdbbc31ca63108d
Myke Stubbs <myke@mykestubbs.com>
2016-12-03 15:22:26 -0800

    Fix ScheduleMessages spec

--- spec/appboy/rest/schedule_messages_spec.rb
+++ spec/appboy/rest/schedule_messages_spec.rb
@@ -6,8 +6,10 @@ describe Appboy::REST::ScheduleMessages do
   let(:payload) {{
     send_at: :send_at,
     segment_id: :segment_id,
-    local_timezone: :local_timezone,
-    messages: :messages
+    external_user_ids: :external_user_ids,
+    campaign_id: :campaign_id,
+    messages: :messages,
+    local_timezone: :in_local_time
   }}
 
   let(:app_group_id) { :app_group_id }
@@ -23,12 +25,16 @@ describe Appboy::REST::ScheduleMessages do
   end
 
   def expect_schedule_messages_http_call
-    expect(http).to receive(:post).with '/messages/schedule', {
+    expect(http).to receive(:post).with('/messages/schedule/create', {
       app_group_id: app_group_id,
-      segment_ids: [:segment_id],
-      send_at: :send_at,
-      deliver_in_local_timezone: :local_timezone,
-      messages: :messages
-    }
+      external_user_ids: :external_user_ids,
+      segment_id: :segment_id,
+      campaign_id: :campaign_id,
+      messages: :messages,
+      schedule: {
+        time: :send_at,
+        in_local_time: :in_local_time
+      }
+    })
   end
 end

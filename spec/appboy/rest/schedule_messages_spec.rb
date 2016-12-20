f673f2deb6e752b3656b6d1146e77615b78ef7a4
Alex Lewis <ebcomalex@gmail.com>
2016-12-19 16:33:16 -0800

    Update Appboy::REST::ScheduleMessages spec to test update endpoint

--- spec/appboy/rest/schedule_messages_spec.rb
+++ spec/appboy/rest/schedule_messages_spec.rb
@@ -2,31 +2,65 @@ require 'spec_helper'
 
 describe Appboy::REST::ScheduleMessages do
   let(:http) { double(:http) }
-   let(:messages) { [build(:messages)] }
+  let(:messages) { [build(:messages)] }
 
-  let(:payload) {{
-    send_at: :send_at,
-    segment_id: :segment_id,
-    external_user_ids: :external_user_ids,
-    campaign_id: :campaign_id,
-    messages: messages,
-    local_timezone: :in_local_time
-  }}
+  let(:create_payload) {
+    {
+      send_at: :send_at,
+      segment_id: :segment_id,
+      external_user_ids: :external_user_ids,
+      campaign_id: :campaign_id,
+      messages: messages,
+      local_timezone: :in_local_time
+    }
+  }
+
+  let(:update_payload) {
+    {
+      send_at: :send_at,
+      schedule_id: :schedule_id,
+      segment_id: :segment_id,
+      external_user_ids: :external_user_ids,
+      campaign_id: :campaign_id,
+      messages: messages,
+      local_timezone: :in_local_time
+    }
+  }
 
   let(:app_group_id) { :app_group_id }
 
-  subject { described_class.new(app_group_id, payload) }
-
   before { subject.http = http }
 
-  it 'makes an http call to the schedule messages endpoint' do
-    expect_schedule_messages_http_call
+  context 'scheduled message creation' do
+    subject { described_class.new(app_group_id, create_payload) }
 
-    subject.perform
+    it 'makes an http call to the schedule messages create endpoint' do
+      expect_schedule_messages_create_http_call
+
+      subject.perform
+    end
   end
 
-  def expect_schedule_messages_http_call
-    expect(http).to receive(:post).with('/messages/schedule/create', {
+  context 'scheduled message update' do
+    subject { described_class.new(app_group_id, update_payload) }
+
+    it 'makes an http call to the schedule messages update endpoint' do
+      expect_schedule_messages_update_http_call
+
+      subject.perform
+    end
+  end
+
+  def expect_schedule_messages_create_http_call
+    expect(http).to receive(:post).with('/messages/schedule/create', post_params)
+  end
+
+  def expect_schedule_messages_update_http_call
+    expect(http).to receive(:post).with('/messages/schedule/update', post_params.merge(schedule_id: :schedule_id))
+  end
+
+  def post_params
+    {
       app_group_id: app_group_id,
       external_user_ids: :external_user_ids,
       segment_id: :segment_id,
@@ -36,6 +70,6 @@ describe Appboy::REST::ScheduleMessages do
         time: :send_at,
         in_local_time: :in_local_time
       }
-    })
+    }
   end
 end
